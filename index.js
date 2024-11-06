582ac3fe9c444edb516f012aa80d703a9fd40326
halazYoha <hailemariamyohannes07@gmail.com>
2024-11-06 10:12:55 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,21 @@
+const check1=document.getElementById('check1');
+const check2=document.getElementById('check2');
+const result=document.getElementById('temp');
+const parr=document.getElementById('par');
+
+function fun(){
+    parr.textContent="select a unit" ;
+ /*   
+   if(check1.checked) 
+    {
+        parr.textContent="select a unit" ;
+    }
+
+   else if(check2.checked){
+    parr.textContent="select a unit" ;
+
+}
+  else {
+    parr.textContent="select a unit" ;
+}*/
+}
